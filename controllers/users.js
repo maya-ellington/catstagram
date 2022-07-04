2ac527b4b0172a6089a9b6e1a32ec720136b1a63
virtual-luna <maya.ellington@berkeley.edu>
2022-07-04 16:52:24 -0700

    user controller cleanup

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,7 @@ const jwt = require("jsonwebtoken");
 const SECRET = process.env.SECRET;
 const { v4: uuidv4 } = require("uuid");
 const S3 = require("aws-sdk/clients/s3");
-const s3 = new S3(); // initialize the construcotr
-// now s3 can crud on our s3 buckets
+const s3 = new S3(); 
 
 module.exports = {
   signup,
@@ -12,34 +11,27 @@ module.exports = {
 };
 
 function signup(req, res) {
+
   console.log(req.body, req.file);
 
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////////////
-
-  // FilePath unique name to be saved to our butckt
   const filePath = `${uuidv4()}/${req.file.originalname}`;
   const params = {
     Bucket: process.env.BUCKET_NAME,
     Key: filePath,
     Body: req.file.buffer,
   };
-  //your bucket name goes where collectorcat is
-  //////////////////////////////////////////////////////////////////////////////////
+  
   s3.upload(params, async function (err, data) {
-    console.log(data, "from aws"); // data.Location is our photoUrl that exists on aws
+    console.log(data, "from aws"); 
     const user = new User({ ...req.body, photoUrl: data.Location });
     try {
       await user.save();
-      const token = createJWT(user); // user is the payload so this is the object in our jwt
+      const token = createJWT(user); // User is the payload so this is the object in jwt
       res.json({ token });
     } catch (err) {
-      // Probably a duplicate email
       res.status(400).json(err);
     }
   });
-  //////////////////////////////////////////////////////////////////////////////////
 }
 
 async function login(req, res) {
@@ -47,7 +39,6 @@ async function login(req, res) {
     const user = await User.findOne({ email: req.body.email });
     console.log(user, " this user in login");
     if (!user) return res.status(401).json({ err: "bad credentials" });
-    // had to update the password from req.body.pw, to req.body password
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (isMatch) {
         const token = createJWT(user);
