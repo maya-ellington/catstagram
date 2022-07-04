81fc7ed744f80fc3d589cc278896e676da9fbfa9
virtual-luna <maya.ellington@berkeley.edu>
2022-07-04 16:39:26 -0700

    updating default user model comments, clean up

--- models/user.js
+++ models/user.js
@@ -15,16 +15,14 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// Remove password property when serializing to JSON
 userSchema.set("toJSON", {
   transform: function (doc, ret) {
-    // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
   },
 });
-/// in controller
 
-// this is if you populate the user
 userSchema.set("toObject", {
   transform: (doc, ret, opt) => {
     delete ret.password;
@@ -32,29 +30,23 @@ userSchema.set("toObject", {
   },
 });
 
-// DO NOT DEFINE instance methods with arrow functions,
-// they prevent the binding of this
+// Password encryption - salt & hash
+
 userSchema.pre("save", function (next) {
-  // 'this' will be set to the current document
   const user = this;
-  // check to see if the user has been modified, if not proceed
-  // in the middleware chain
   if (!user.isModified("password")) return next();
-  // password has been changed - salt and hash it
   bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
     if (err) return next(err);
-    // replace the user provided password with the hash
     user.password = hash;
     next();
   });
 });
 
+// Password Match
 userSchema.methods.comparePassword = function (tryPassword, cb) {
   console.log(cb, " this is cb");
-  // 'this' represents the document that you called comparePassword on
   bcrypt.compare(tryPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
-
     cb(null, isMatch);
   });
 };
