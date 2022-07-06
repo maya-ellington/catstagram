6a18eb2739ee2bebcf49291254e4b07799e94509
virtual-luna <maya.ellington@berkeley.edu>
2022-07-05 19:43:59 -0700

    added login page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -58,7 +58,7 @@ export default function SignUpPage(props) {
 
   return (
     <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 300 }}>
+      <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2" color="black" textAlign="center">
           <Image src="https://i.imgur.com/lrk0Rja.jpeg" /> Sign Up
         </Header>
