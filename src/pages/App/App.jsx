37c6e4fae3814de14149f67870df932614e89bc3
virtual-luna <maya.ellington@berkeley.edu>
2022-07-07 21:06:03 -0700

    clean up

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -8,9 +8,9 @@ import FeedPage from "../FeedPage/FeedPage";
 
 
 function App() {
-  const [user, setUser] = useState(userService.getUser()); // getUser decodes our JWT token, into a javascript object
-  // this object corresponds to the jwt payload which is defined in the server signup or login function that looks like
-  // this  const token = createJWT(user); // where user was the document we created from mongo
+  const [user, setUser] = useState(userService.getUser()); 
+  //getUser decodes jwttoken
+  // this object corresponds to the jwt payload which is defined in the server signup or login function 
 
   function handleSignUpOrLogin() {
     setUser(userService.getUser()); // getting the user from localstorage decoding the jwt
